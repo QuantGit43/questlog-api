fd749d1e41c2d3d2b1bc2567933d7ab83f5ef8de
Bileysh <bileysh06@knu.ua>
2025-12-29 12:35:04 +0200

    Add Validators

---
+++ QuestLog.Application/Feature/Tasks/Validators/UpdateTaskCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using QuestLog.Application.Feature.Tasks.Commands;
+
+namespace QuestLog.Application.Feature.Tasks.Validators;
+
+public class UpdateTaskCommandValidator: AbstractValidator<UpdateTaskCommand>
+{
+    public UpdateTaskCommandValidator()
+    {
+        RuleFor(x => x.TaskId)
+            .NotEqual(Guid.Empty).WithMessage("Некоректний ID завдання.");
+        RuleFor(x => x.Title)
+            .NotEmpty().WithMessage("Назва завдання обов'язкова")
+            .MaximumLength(100).WithMessage("Назва завдання занадто довга");
+        RuleFor(x => x.XPReward)
+            .GreaterThanOrEqualTo(0).WithMessage("Винагорода за досвід не може бути від'ємною");
+        RuleFor(x => x.GoldReward)
+            .GreaterThanOrEqualTo(0).WithMessage("Винагорода за золото не може бути від'ємною");
+        RuleFor(x => x.Description)
+            .MaximumLength(500).WithMessage("Опис завдання занадто довгий"); }
+}
---
+++ QuestLog.Application/Feature/Users/Validators/UpdateUserCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+using FluentValidation;
+using QuestLog.Application.Feature.Users.Commands;
+
+namespace QuestLog.Application.Feature.Users.Validators;
+
+public class UpdateUserCommandValidator: AbstractValidator<UpdateUserCommand>
+{
+    public UpdateUserCommandValidator()
+    {
+        RuleFor(x => x.UserId)
+            .NotEqual(Guid.Empty).WithMessage("Не вказано ID користувача.");
+
+        RuleFor(x => x.Username)
+            .NotEmpty().WithMessage("Ім'я користувача не може бути порожнім.")
+            .Length(3, 50).WithMessage("Ім'я має бути від 3 до 50 символів.");
+        
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email не може бути порожнім.")
+            .EmailAddress().WithMessage("Некоректний формат Email.");
+        
+    }
+}
